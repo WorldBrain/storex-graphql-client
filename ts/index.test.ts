d799b39651faff5c5c334d0e1a98c9e89ae14ef9
Vincent den Boer <vincent@shishkabab.net>
2019-03-15 11:17:57 +0100

    Prepare for integration testing

--- ts/index.test.ts
+++ ts/index.test.ts
@@ -1,7 +1,7 @@
 import * as expect from 'expect'
 import { StorexGraphQLClient } from '.';
-import { StorageRegistry } from '@worldbrain/storex';
-import { StorageModuleConfig, StorageModuleInterface, registerModuleMapCollections } from '@worldbrain/storex-pattern-modules';
+import { StorageRegistry, CollectionDefinitionMap } from '@worldbrain/storex';
+import { StorageModuleConfig, StorageModuleInterface, registerModuleMapCollections, PublicMethodDefinitions, PublicMethodDefinition, StorageModuleCollections } from '@worldbrain/storex-pattern-modules';
 
 describe('StorexGraphQLClient', () => {
     async function setupTest(options : { modules : {[name : string] : StorageModuleInterface }, respond : (...args) => Promise<any>}) {
@@ -15,6 +15,31 @@ describe('StorexGraphQLClient', () => {
             return options.respond(args)
         }
         return { client, queries }
+    }
+
+    async function runTest(options : {
+        collections : StorageModuleCollections,
+        methodDefinition : PublicMethodDefinition,
+        methodImplementation : (...args) => Promise<any>,
+        callArgs : any[]
+        expectedQuery : any
+    }) {
+        class TestModule implements StorageModuleInterface {
+            getConfig = () : StorageModuleConfig => ({
+                collections: options.collections,
+                methods: {
+                    testMethod: options.methodDefinition,
+                }
+            })
+        }
+        let expectedResponse
+        const { client, queries } = await setupTest({
+            modules: { test: new TestModule() },
+            respond: async (...args) => ({ data: { test: { testMethod: (expectedResponse = await options.methodImplementation(...args)) } } })
+        })
+        const result = await client.getModules().test.testMethod(...options.callArgs)
+        expect(queries).toEqual([[options.expectedQuery]])
+        expect(result).toEqual(expectedResponse)
     }
 
     it('should correctly execute queries', async () => {
@@ -41,243 +66,157 @@ describe('StorexGraphQLClient', () => {
     })
 
     it('should correctly generate read-only queries', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: { },
-                methods: {
-                    testMethod: { type: 'query', args: { name: 'string' }, returns: 'int' },
-                }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: 5 } } })
+        await runTest({
+            collections: {},
+            methodDefinition: { type: 'query', args: { name: 'string' }, returns: 'int' },
+            methodImplementation: async () => 5,
+            callArgs: [{name: 'John'}],
+            expectedQuery: { query: `{ test { testMethod(name: "John") } }`, variables: {}, type: 'query' }
         })
-        const result = await client.getModules().test.testMethod({name: 'John'})
-        expect(queries).toEqual([[{ query: `{ test { testMethod(name: "John") } }`, variables: {}, type: 'query' }]])
-        expect(result).toEqual(5)
     })
 
     it('should correctly generate read-only queries with positional args', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: { },
-                methods: {
-                    testMethod: {
-                        type: 'query',
-                        args: {
-                            first: { type: 'string', positional: true },
-                            second: { type: 'string', positional: true },
-                            third: { type: 'string' },
-                        },
-                        returns: 'int',
-                    },
-                }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: 5 } } })
+        await runTest({
+            collections: {},
+            methodDefinition: {
+                type: 'query',
+                args: {
+                    first: { type: 'string', positional: true },
+                    second: { type: 'string', positional: true },
+                    third: { type: 'string' },
+                },
+                returns: 'int',
+            },
+            methodImplementation: async () => 5,
+            callArgs: ['foo', 'bar', { third: 'eggs' }],
+            expectedQuery: { query: `{ test { testMethod(first: "foo", second: "bar", third: "eggs") } }`, variables: {}, type: 'query' }
         })
-        const result = await client.getModules().test.testMethod('foo', 'bar', { third: 'eggs' })
-        expect(queries).toEqual([[{
-            query: `{ test { testMethod(first: "foo", second: "bar", third: "eggs") } }`,
-            variables: {}, type: 'query'
-        }]])
-        expect(result).toEqual(5)
     })
 
     it('should correctly select a collection return value', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: {
-                    user: {
-                        version: new Date(),
-                        fields: {
-                            displayName: { type: 'string' },
-                            age: { type: 'int' },
-                        }
+        await runTest({
+            collections: {
+                user: {
+                    version: new Date(),
+                    fields: {
+                        displayName: { type: 'string' },
+                        age: { type: 'int' },
                     }
-                },
-                methods: {
-                    testMethod: {
-                        type: 'query',
-                        args: {},
-                        returns: { collection: 'user' },
-                    },
                 }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: { displayName: 'Joe', age: 30 } } } })
+            },
+            methodDefinition: { type: 'query', args: {}, returns: { collection: 'user' }, },
+            methodImplementation: async () => ({ displayName: 'Joe', age: 30 }),
+            callArgs: [],
+            expectedQuery: { query: `{ test { testMethod { displayName, age, id } } }`, variables: {}, type: 'query' }
         })
-        const result = await client.getModules().test.testMethod()
-        expect(queries).toEqual([[{ query: `{ test { testMethod { displayName, age, id } } }`, variables: {}, type: 'query' }]])
-        expect(result).toEqual({ displayName: 'Joe', age: 30 })
     })
 
     it('should correctly select a collection array return value', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: {
-                    user: {
-                        version: new Date(),
-                        fields: {
-                            displayName: { type: 'string' },
-                            age: { type: 'int' },
-                        }
+        await runTest({
+            collections: {
+                user: {
+                    version: new Date(),
+                    fields: {
+                        displayName: { type: 'string' },
+                        age: { type: 'int' },
                     }
-                },
-                methods: {
-                    testMethod: {
-                        type: 'query',
-                        args: {},
-                        returns: { array: { collection: 'user' } },
-                    },
                 }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: [
+            },
+            methodDefinition: { type: 'query', args: {}, returns: { array: { collection: 'user' } }, },
+            methodImplementation: async () => [
                 { displayName: 'Joe', age: 30 },
                 { displayName: 'Bob', age: 40 },
-            ] } } })
+            ],
+            callArgs: [],
+            expectedQuery: { query: `{ test { testMethod { displayName, age, id } } }`, variables: {}, type: 'query' }
         })
-        const result = await client.getModules().test.testMethod()
-        expect(queries).toEqual([[{ query: `{ test { testMethod { displayName, age, id } } }`, variables: {}, type: 'query' }]])
-        expect(result).toEqual([
-            { displayName: 'Joe', age: 30 },
-            { displayName: 'Bob', age: 40 },
-        ])
     })
 
     it('should correctly handle a scalar array return value', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: { },
-                methods: {
-                    testMethod: { type: 'query', args: { name: 'string' }, returns: { array: 'int' } },
-                }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: [5, 7, 3] } } })
+        await runTest({
+            collections: {},
+            methodDefinition: { type: 'query', args: { name: 'string' }, returns: { array: 'int' } },
+            methodImplementation: async () => [5, 7, 3],
+            callArgs: [{name: 'John'}],
+            expectedQuery: { query: `{ test { testMethod(name: "John") } }`, variables: {}, type: 'query' }
         })
-        const result = await client.getModules().test.testMethod({ name: 'John' })
-        expect(queries).toEqual([[{ query: `{ test { testMethod(name: "John") } }`, variables: {}, type: 'query' }]])
-        expect(result).toEqual([5, 7, 3])
     })
 
     it('should correctly handle void return values', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: { },
-                methods: {
-                    testMethod: { type: 'query', args: { name: 'string' }, returns: 'void' },
-                }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: null } } })
+        await runTest({
+            collections: {},
+            methodDefinition: { type: 'query', args: { name: 'string' }, returns: 'void' },
+            methodImplementation: async () => null,
+            callArgs: [{ name: 'John' }],
+            expectedQuery: { query: `{ test { testMethod(name: "John") { void } } }`, variables: {}, type: 'query' }
         })
-        const result = await client.getModules().test.testMethod({ name: 'John' })
-        expect(queries).toEqual([[{ query: `{ test { testMethod(name: "John") { void } } }`, variables: {}, type: 'query' }]])
-        expect(result).toEqual(null)
     })
 
     it('should be able to pass collections as arguments', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: {
-                    user: {
-                        version: new Date(),
-                        fields: {
-                            displayName: { type: 'string' },
-                            age: { type: 'int' },
-                        }
+        await runTest({
+            collections: {
+                user: {
+                    version: new Date(),
+                    fields: {
+                        displayName: { type: 'string' },
+                        age: { type: 'int' },
                     }
-                },
-                methods: {
-                    testMethod: {
-                        type: 'query',
-                        args: { user: { collection: 'user' } },
-                        returns: 'string',
-                    },
                 }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: 5 } } })
+            },
+            methodDefinition: {
+                type: 'query',
+                args: { user: { collection: 'user' } },
+                returns: 'string',
+            },
+            methodImplementation: async () => 5,
+            callArgs: [{ user: { displayName: 'Joe', age: 30 } }],
+            expectedQuery: {
+                query: `{ test { testMethod(user: $user) } }`,
+                variables: { user: { displayName: 'Joe', age: 30 } },
+                type: 'query',
+            }
         })
-        const result = await client.getModules().test.testMethod({ user: { displayName: 'Joe', age: 30 } })
-        expect(queries).toEqual([[{
-            query: `{ test { testMethod(user: $user) } }`,
-            variables: { user: { displayName: 'Joe', age: 30 } },
-            type: 'query',
-        }]])
-        expect(result).toEqual(5)
     })
 
     it('should be able to pass collection arrays as arguments', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: {
-                    user: {
-                        version: new Date(),
-                        fields: {
-                            displayName: { type: 'string' },
-                            age: { type: 'int' },
-                        }
+        await runTest({
+            collections: {
+                user: {
+                    version: new Date(),
+                    fields: {
+                        displayName: { type: 'string' },
+                        age: { type: 'int' },
                     }
-                },
-                methods: {
-                    testMethod: {
-                        type: 'query',
-                        args: { users: { array: { collection: 'user' } } },
-                        returns: 'string',
-                    },
                 }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: 5 } } })
-        })
-        const result = await client.getModules().test.testMethod({ users: [
-            { displayName: 'Joe', age: 30 },
-            { displayName: 'Bob', age: 40 },
-        ] })
-        expect(queries).toEqual([[{
-            query: `{ test { testMethod(users: $users) } }`,
-            variables: { users: [
+            },
+            methodDefinition: {
+                type: 'query',
+                args: { users: { array: { collection: 'user' } } },
+                returns: 'string',
+            },
+            methodImplementation: async () => 5,
+            callArgs: [{ users: [
                 { displayName: 'Joe', age: 30 },
                 { displayName: 'Bob', age: 40 },
-            ] },
-            type: 'query',
-        }]])
-        expect(result).toEqual(5)
+            ] }],
+            expectedQuery: {
+                query: `{ test { testMethod(users: $users) } }`,
+                variables: { users: [
+                    { displayName: 'Joe', age: 30 },
+                    { displayName: 'Bob', age: 40 },
+                ] },
+                type: 'query',
+            }
+        })
     })
 
     it('should be able to execute mutations', async () => {
-        class TestModule implements StorageModuleInterface {
-            getConfig = () : StorageModuleConfig => ({
-                collections: { },
-                methods: {
-                    testMethod: { type: 'mutation', args: { name: 'string' }, returns: 'void' },
-                }
-            })
-        }
-        const { client, queries } = await setupTest({
-            modules: { test: new TestModule() },
-            respond: async () => ({ data: { test: { testMethod: null } } })
+        await runTest({
+            collections: {},
+            methodDefinition: { type: 'mutation', args: { name: 'string' }, returns: 'void' },
+            methodImplementation: async () => null,
+            callArgs: [{ name: 'John' }],
+            expectedQuery: { query: `mutation { test { testMethod(name: "John") { void } } }`, variables: {}, type: 'mutation' }
         })
-        const result = await client.getModules().test.testMethod({ name: 'John' })
-        expect(queries).toEqual([[{ query: `mutation { test { testMethod(name: "John") { void } } }`, variables: {}, type: 'mutation' }]])
-        expect(result).toEqual(null)
     })
 })
